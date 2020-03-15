63a7101c24bc86cd4dedbadeb97d4c458fce8fa3
vishal <vishal.p@borqs.com>
2020-03-15 19:54:54 +0530

    Setup HomeViewController TableView and UISetup

--- NewTestIOS/HomeViewController.swift
+++ NewTestIOS/HomeViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+   
+    //MARK:- UI Compenents
+    var tableInfoList = UITableView()
+    fileprivate let cellId = "cellId"
+    
+    //MARK:- Data Variables
+    var arrayInfoList:[String] = ["aa", "bb"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //MARK:- Set UI for views
+    func setUpUIForViews() -> Void {
+       // let refresh = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.refresh, target: self, action: #selector(fetchDataFromAPI))
+       // self.navigationItem.rightBarButtonItem  = refresh
+    
+        view.backgroundColor = .white
+        tableInfoList.delegate = self
+        tableInfoList.dataSource = self
+        tableInfoList.frame = view.frame
+        tableInfoList.register(HomeInfoTableViewCell.self, forCellReuseIdentifier: cellId)
+        view.addSubview(tableInfoList)
+    }
 
 
+}
+
+extension HomeViewController:UITableViewDelegate,UITableViewDataSource{
+    
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayInfoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableInfoList.dequeueReusableCell(withIdentifier: cellId) as! HomeInfoTableViewCell
+        //let obj = arrayInfoList[indexPath.row]
+        cell.textLabel?.text = arrayInfoList[indexPath.row]
+        //cell.setData(data: obj)
+        return cell
+    }
+    
 }
 

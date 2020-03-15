946d01e578b969fc27d806536c3e62c4df05d723
vishal <vishal.p@borqs.com>
2020-03-15 20:14:08 +0530

    DataModel and Constant File Added -- DataModel added for JSON response feed -- Conform Models to Codable Protocol -- Base Url path added for getting JSON Data -- Added keys

---
+++ NewTestIOS/Constant/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  NewTestIOS
+//
+//  Created by Borqs India on 15/03/20.
+//  Copyright © 2020 Vishal. All rights reserved.
+//
+
+import Foundation
+
+let BaseUrlPath = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
---
+++ NewTestIOS/DataModel/DataModel.swift
@@ -0,0 +1,33 @@
+//
+//  DataModel.swift
+//  NewTestIOS
+//
+//  Created by Borqs India on 15/03/20.
+//  Copyright © 2020 Vishal. All rights reserved.
+//
+
+import Foundation
+
+//Data Model for JSON Response
+struct DataInfo: Codable {
+    
+    let title: String?
+    let rows: [RowInfo]
+
+    private enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case rows = "rows"
+    }
+}
+
+struct RowInfo: Codable {
+    let title: String?
+    let description: String?
+    let imageHref: String?
+    
+    private enum CodingKeys: String, CodingKey{
+        case title = "title"
+        case description = "description"
+        case imageHref = "imageHref"
+    }
+}
